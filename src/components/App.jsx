666d5f0bd547dbc50f4a7725f2830dac8c691e19
romfantast <romfantast@gmail.com>
2023-07-22 14:40:04 +0300

    add tailwind

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,3 @@
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
